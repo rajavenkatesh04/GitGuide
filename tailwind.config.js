d0fa0e0a4b272e236057e80be4034fd8a13bc45f
Guthula Raja venkatesh <rg3941@srmist.edu.in>
2024-12-13 01:32:04 +0530

    Fix Tailwind config for ESM compatibility and set Node.js version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
-// tailwind.config.js
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "grid-gradient":
-          "linear-gradient(135deg, #333 25%, transparent 25%), linear-gradient(225deg, #333 25%, transparent 25%), linear-gradient(45deg, #333 25%, transparent 25%), linear-gradient(-45deg, #333 25%, transparent 25%)",
-      },
-      colors: {
-        "dark-gray": "#1A1A1A", // You can use any custom gray
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
